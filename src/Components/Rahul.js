82648842238e2935598c684c27c3fcb6991560fc
perupoguvenkatesh <perupoguvenkatesh5073@gmail.com>
2026-03-04 12:05:01 +0530

    merge conflict pv and rahul components

--- src/Components/Rahul.js
+++ src/Components/Rahul.js
@@ -1,10 +1,10 @@
 function Rahul(){
     return(
         <div>
-            <h1>he is charan</h1>
+            <h1>he is PV</h1>
             <h1>kanre brajesh rahul</h1>
             <h1>100+22</h1>
-            <h1>0+44</h1>
+            <h1>This is the End Heading</h1>
         </div>
     )
 }
