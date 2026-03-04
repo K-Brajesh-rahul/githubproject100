39bcba9e690f1b184ca495f535535fcd02478fe0
perupoguvenkatesh <perupoguvenkatesh5073@gmail.com>
2026-03-04 11:55:58 +0530

    charan commit

--- src/Components/Rahul.js
+++ src/Components/Rahul.js
@@ -1,9 +1,9 @@
 function Rahul(){
     return(
         <div>
-            <h1>This is Rahul</h1>
+            <h1>he is Rahul</h1>
             <h1>kanre brajesh rahul</h1>
-            <h1>100+100</h1>
+            <h1>0+100</h1>
             <h1>0+44</h1>
         </div>
     )
