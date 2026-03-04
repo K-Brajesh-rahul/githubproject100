7ee9eeac0624acc51fac03cd308090a516893322
beastpluse <brajeshrahul123@gmail.com>
2026-03-04 12:18:16 +0530

    qqq

--- src/Components/Rahul.js
+++ src/Components/Rahul.js
@@ -1,13 +1,9 @@
 function Rahul(){
     return(
         <div>
-<<<<<<< HEAD
-            <h1>he is hhhhh</h1>
-=======
-            <h1>he is PV</h1>
->>>>>>> 3ab0494e33e5967e45986fb9a12536b210c7df23
+            <h1>he is Venkatesh</h1>
             <h1>kanre brajesh rahul</h1>
-            <h1>jbjbjbj+22</h1>
+            <h1>100+22</h1>
             <h1>0+21</h1>
         </div>
     )
