3d2e4f5ffee0d212f2cd0962983176acf03f1b95
beastpluse <brajeshrahul123@gmail.com>
2026-03-04 12:02:35 +0530

    done

--- src/Components/Rahul.js
+++ src/Components/Rahul.js
@@ -1,10 +1,10 @@
 function Rahul(){
     return(
         <div>
-            <h1>he is charan</h1>
+            <h1>he is Venkatesh</h1>
             <h1>kanre brajesh rahul</h1>
             <h1>100+22</h1>
-            <h1>0+44</h1>
+            <h1>0+21</h1>
         </div>
     )
 }
